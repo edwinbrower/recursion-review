d174cbc1f6cf9f054be863164a8429d21bb34ede
edwinbrower <edwin.brower@gmail.com>
2017-02-06 12:28:52 -0800

    getElementsByClassName mvp

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,14 +5,15 @@
 //You should use document.body, element.childNodes, and element.classList
 // But instead we're going to implement it from scratch:
 // [body.targetClassName, div.targetClassName]
+
 var getElementsByClassName = function(className) {
   var results = [];
-  var findClass=function(node){
-    if(node.classList.contains('className')) {
+  var findClass = function(node) {
+    if (node.classList !== undefined && node.classList.contains(className)) {
       results.push(node);
     }
     if (node.childNodes.length) {
-      node.childNodes.each(findClass());
+      node.childNodes.forEach(findClass);
     }
   };
   findClass(document.body);
