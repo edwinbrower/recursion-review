532fc6d9ba8f4620a65ed162640eb017a336a8e0
edwinbrower <edwin.brower@gmail.com>
2017-02-06 21:06:35 -0800

    Add ability to parse empty arrays

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -14,17 +14,32 @@ var parseJSON = function(json) {
   /*if (typeof json !== 'string') {
     return 'SyntaxError';
   }*/
+  var start, end;
+  //var end;
   for (var i = 0; i < json.length; i++) {
     if (json[i] === '[') {
-      var start = i;
+      start = i;
     }
+    break;
+
     //[1,2,["hi"]];
       /*for (var j = i; json[i] !== ']'; j++) {
         parseJSON(j);
       }*/
     //return [];
   }
-  
+  for (var i = json.length - 1; i > 0; i--) {
+    if (json[i] === ']') {
+      end = i;
+    }
+    break;
+  }
+  var arr = [];
+  if (json.slice(start + 1, end).length) {
+    arr.push(json.slice(start + 1, end).split(', '));
+  }
+  return arr;
+  //return json.slice(start + 1, end);
 
 
 
