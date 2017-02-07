9282d39a27051960a2f77d5134e72ae34d9965ae
edwinbrower <edwin.brower@gmail.com>
2017-02-06 20:09:45 -0800

    Parse attempt

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,21 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  //debugger;
+  /*var stringed = '"' + json + '"';
+  for (var i = 1; i < stringed.length - 1; i++) {
+    if (json[i] === '\'') {
+      json[i] = '';
+    }
+  }
+  return json;*/
+  if (typeof json !== 'string') {
+    return 'SyntaxError';
+  }
+  for (var i = 0; i < json.length; i++) {
+    if (json[i] === '[') {
+      return [];
+    }
+  }
+  //return json;
 };
