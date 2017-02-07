19a7923f9844f9ef0a2446a5c8d4c9806434b4ce
edwinbrower <edwin.brower@gmail.com>
2017-02-06 16:02:54 -0800

    Complete stringifyJSON and stringifyJSONspec

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,27 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //debugger;
+  if (obj === null) {
+    return '' + obj;
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    return '[' + obj.map(stringifyJSON) + ']';
+  }
+  if (typeof obj === 'object') {
+
+    var objArray = [];
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || obj[key] === undefined) {
+        continue;
+      } else { 
+        objArray.push('"' + key + '"' + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + objArray + '}';
+  }
+  return '' + obj;
 };
